0feff0f772c5557bd3acf9ca1783398d82c7ecea
Ahmed7guizani <ahmed.guizani@outlook.com>
2020-05-18 00:27:09 +0200

    splash screen finished!!!

--- app/src/main/java/com/example/td3_ahmed_guizani/SplashScreenActivity.java
+++ app/src/main/java/com/example/td3_ahmed_guizani/SplashScreenActivity.java
@@ -1,14 +1,30 @@
 package com.example.td3_ahmed_guizani;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private final int SPLASH8SCREEN8TIMEOUT = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //rediriger vers la liste des pokemon apres 5 secondes
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // passer a la fragment de list des pokemon
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH8SCREEN8TIMEOUT);
+
     }
 }
