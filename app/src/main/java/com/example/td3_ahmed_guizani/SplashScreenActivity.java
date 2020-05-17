9046ad20c2393aa1a73437ba7c61db851de431dc
Ahmed7guizani <ahmed.guizani@outlook.com>
2020-05-17 21:15:54 +0200

    splashscreen inprocess !!!!!!

---
+++ app/src/main/java/com/example/td3_ahmed_guizani/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.td3_ahmed_guizani;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
